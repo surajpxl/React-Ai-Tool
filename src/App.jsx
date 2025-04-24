9a8873dc0b3e83ef1f46d970ee9b2cfc55effb46
surajpxl <guptasuraj4455@gmail.com>
2025-04-24 20:10:58 +0530

    api integration  completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
 import { useState } from 'react'
+import { URL } from './constants'
 
 function App() {
   const [question, setQuestion] =useState('')
-  const askQuestion = () => {
-    console.log(question);
-    
+  const [result, setResult] = useState(undefined)
+
+  const payload = {
+    "contents": [{
+      "parts":[{"text": question}]
+      }]
+     }
+
+  const askQuestion = async() => {
+    let response = await fetch(URL,{
+      method: 'POST',
+      body: JSON.stringify(payload),
+    })
+    response  = await response.json()
+    // console.log(response.candidates[0].content.parts[0].text);
+    setResult(response.candidates[0].content.parts[0].text);
   }
 
   return (
@@ -15,8 +29,10 @@ function App() {
       </div>
 
       <div className='col-span-4 p-10'>
-        <div className='container h-110'>
-
+        <div className='container h-110 overflow-scroll' style={{ scrollbarWidth: 'none', msOverflowStyle: 'none' }}>
+          <div className='text-white'>
+            {result}
+          </div>
         </div>
         <div className='bg-zinc-800 w-1/2 p-1 pr-5 text-white m-auto rounded-4xl border border-zinc-700 flex h-16'>
           <input type="text" value={question} onChange={(event)=>setQuestion(event.target.value)} className='w-full h-full p-3 outline-none' placeholder='Ask me anything' />
---
+++ src/constants.js
@@ -0,0 +1,1 @@
+export const URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyD86KEpSWJfumvO1obluWC_YsxNV1ZN7BY"
